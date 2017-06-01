932f4456e9b1dd6b90077097503b4459109f14cb
tvnojohn <b1012038@fun.ac.jp>
2017-06-01 15:12:10 +0900

    fix file name

--- upload_temp.sh
+++ upload_temp.sh
@@ -4,4 +4,4 @@ FILENAME=$(date +"%Y-%m-%d")
 
 bash /usr/local/bin/dropbox_uploader.sh upload /home/jun/pi_temp.txt $FILENAME/temp.txt
 
-echo '' > /home/jun/temp.txt
+echo '' > /home/jun/pi_temp.txt
